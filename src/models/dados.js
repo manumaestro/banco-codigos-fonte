632714a06bfec062bf84af5a76bcae3f5c38643d
manumaestro <manuela.maestro@aluno.senai.br>
2025-09-22 14:07:08 -0300

    feat: Preenchendo controller.js

---
+++ src/models/dados.js
@@ -0,0 +1,59 @@
+export const registros = [
+    {
+      id: 1,
+      nome: "Linux",
+      linguagem: "Assembly",
+      autor: "Eich",
+      licenca: "GNU GPL",
+      estrelas: 5,
+      forks: 1000,
+      categoria: "Ubunto",
+      dataAtualizacao: "03/2024"
+    },
+    {
+      id: 2,
+      nome: "React",
+      linguagem: "JavaScript",
+      autor: "Meta",
+      licenca: "MIT",
+      estrelas: 230000,
+      forks: 46000,
+      categoria: "Framework",
+      dataAtualizacao: "09/2025"
+    },
+    {
+      id: 3,
+      nome: "TensorFlow",
+      linguagem: "Python",
+      autor: "Google",
+      licenca: "Apache 2.0",
+      estrelas: 180000,
+      forks: 91000,
+      categoria: "Machine Learning",
+      dataAtualizacao: "09/2025"
+    },
+    {
+      id: 4,
+      nome: "Vue.js",
+      linguagem: "JavaScript",
+      autor: "Evan You",
+      licenca: "MIT",
+      estrelas: 200000,
+      forks: 34000,
+      categoria: "Framework",
+      dataAtualizacao: "09/2025"
+    },
+    {
+      id: 5,
+      nome: "Django",
+      linguagem: "Python",
+      autor: "Django Software Foundation",
+      licenca: "BSD 3-Clause",
+      estrelas: 75000,
+      forks: 31000,
+      categoria: "Framework",
+      dataAtualizacao: "09/2025"
+    }
+  ];
+
+export default registros;
